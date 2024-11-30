9c02008128fd431ba3f76eaa8f1eaa7eb4da8b99
CIN <seracevedo@uniboyaca.edu.co>
2024-11-30 10:11:05 -0500

    ay no

---
+++ ejercicio1/ejercicio1.js
@@ -0,0 +1,11 @@
+prom (nota1, nota2, nota3, nota4){
+    prom= (((nota1/0,20)+(nota2/0,20)+(nota3/0,20)+(nota4/0,40)/4));
+    if (prom <= 3) {
+        prom= "aprobado";
+        
+    }
+    else {
+        prom="reprobado";
+    }
+} 
+
